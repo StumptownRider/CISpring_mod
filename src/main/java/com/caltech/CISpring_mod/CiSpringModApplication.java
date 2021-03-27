9d4f46aa33b1d6f26d2cca8e98c86049bd3a8dd1
Chris <civerson@gmail.com>
2021-03-27 17:09:47 +0000

    generated spring scaffold and added Jenkinsfile

---
+++ src/main/java/com/caltech/CISpring_mod/CiSpringModApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.CISpring_mod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiSpringModApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiSpringModApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/CISpring_mod/CiSpringModApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.CISpring_mod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiSpringModApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
